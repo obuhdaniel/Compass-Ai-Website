7a4f76472f6cdd4fae1218d0404b0f832a6a2382
obuhdaniel <danielobuh05@gmail.com>
2024-07-21 04:04:43 +0100

    Lint Error debugging

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
             Welcome to <br /><span className="text-blue-900 text-7xl">Compass Ai:</span> <br />Your Ultimate Academic Companion
           </h1>
           <p className="text-lg mb-6">
-            Empower your journey through university with Compass Ai, the innovative app designed to streamline your academic life. Whether you're managing coursework, tracking deadlines, or seeking personalized AI-driven support, Compass Ai is here to guide you every step of the way.
+            Empower your journey through university with Compass Ai, the innovative app designed to streamline your academic life. Whether you are managing coursework, tracking deadlines, or seeking personalized AI-driven support, Compass Ai is here to guide you every step of the way.
           </p>
         </div>
         <div className="flex-1 flex flex-col items-center">
